cb7acd1a1c48786814685ec13b0751bc5685479b
Wiirlak <36702513+Wiirlak@users.noreply.github.com>
2019-04-07 19:33:00 +0200

    Revert "trying shit for postgre"
    This reverts commit 217dce0845aa0b966b20f6c05499595210cd49f2.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,25 +24,6 @@ class UserController {
         return await User.findAll();
     }
 
-
-     async passwordHash(pwd) {
-        const bcrypt = require('bcrypt');
-        const saltRounds = 10;
-        bcrypt.hash(pwd, saltRounds, function(err, hash) {
-            // Store hash in your password DB.
-        });
-    }
-
-    async checkUser(usr, pwd) {
-        const match = await bcrypt.compare(pwd, usr.password);
-
-        if(match) {
-            //login
-        }
-
-        //...
-    }
-
 }
 
 module.exports = new UserController();
--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 'use strict';
-const { Pool } = require('pg');
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
-require('dotenv').config();
 const RouterBuilder = require('./routes');
 
 const app = express();
 app.use(morgan('dev'));
 
-const pool = new Pool({
-    connectionString: process.env.DB_URL
-});
 RouterBuilder.build(app);
 
-pool.on('connect', () => {
-    console.log("Connected to the db");
-});
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Fortnite starting on port ${port}...`));
